b020c2601350b5303261f5536d0ac32eacd659d8
Sabarish Sridhar <sabarish.sr@outlook.com>
2024-08-06 07:42:38 +0200

    Add support for displaying index.html using rocket

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::{FileServer, relative};
+use rocket::response::Redirect;
+mod manual {
+    use std::path::{PathBuf, Path};
+    use rocket::fs::NamedFile;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, From Inventory Manager!"
+    #[rocket::get("/second/<path..>")]
+    pub async fn second(path: PathBuf) -> Option<NamedFile> {
+        let mut path = Path::new(super::relative!("templates")).join(path);
+        if path.is_dir() {
+            path.push("index.html");
+        }
+
+        NamedFile::open(path).await.ok()
+     
+    }
 }
 
-#[launch]
+
+
+#[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", rocket::routes![manual::second])
+        .mount("/", FileServer::from(relative!("templates")))
 }
+
+
