d474855054e5cb3b9e1d79674882cb127b32114c
Sabarish Sridhar <sabarish.sr@outlook.com>
2025-06-28 08:19:41 +0200

    Serialized logging

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ CORS(app)
 
 # Set up logging
 logger = loguru.logger
+logger.add("debugLogs.log", format="{time} {level} {message}", level="DEBUG", serialize=True)
 
 # Define routes
 @app.route("/")
