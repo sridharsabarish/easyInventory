0c46a285dc8bc3741c89c5c401089f10d7e419c8
Sabarish Sridhar <sabarish.sr@outlook.com>
2025-07-02 20:45:41 +0200

    Some more unit tests

--- test/test_links_up.py
+++ test/test_links_up.py
@@ -1,11 +1,21 @@
-def test_dummy():
-    assert 1== 1
-    
-import requests
 
-def test_ping():
-    try:
-        response = requests.head('http://localhost:5000')
-    except requests.ConnectionError:
-        assert False, "Connection Error"
-    assert response.status_code == 200
+
+import requests
+class TestLinks:
+    def test_dummy(self):
+        assert 1== 1
+        
+    import requests
+
+    def test_ping(self):
+        try:
+            response = requests.head('http://localhost:5000')
+        except requests.ConnectionError:
+            assert False, "Connection Error"
+        assert response.status_code == 200
+    def test_ping_google(self):
+        try:
+            response = requests.head('https://www.google.co.in')
+        except requests.ConnectionError:
+            assert False, "Connection Error"
+        assert response.status_code == 200
