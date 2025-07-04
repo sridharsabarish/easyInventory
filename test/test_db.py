aacfdbd4070bd1fba949345ec2eca343ec602b25
Sabarish Sridhar <sabarish.sr@outlook.com>
2025-07-04 07:52:22 +0200

    Added pytest to import to fix github action failure

--- test/test_db.py
+++ test/test_db.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from classes.Item import Item
 from classes.DB import DB
